5046b6136f047fb014dde7f2a99b360378c09162
hnsblnbrg <hansibrahimasi@gmail.com>
2021-08-09 19:02:34 +0200

    Add API Endpoints for genres, playlists, tracks

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -19,4 +19,77 @@ const APIController = (function () {
     const data = await result.json();
     return data.access_token;
   };
+
+  //API Endpoints
+
+  //Get a list of genres
+  const getGenres = async (token) => {
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    });
+
+    const data = await result.json();
+    return data.categories.items;
+  };
+
+  //Get a lists of playlists for selected genre
+  const getPlaylistsForGenre = async (token, genreID) => {
+    //Get only 10 playlists
+    const limit = 10;
+
+    const result = await fetch(
+      `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
+      {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+      }
+    );
+
+    const data = await result.json();
+    return data.playlists.items;
+  };
+
+  //Get tracks for selected playlist
+  const getTracksForPlaylist = async (token, tracksEndPoint) => {
+    //Get only 10 tracks
+    const limit = 10;
+
+    const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    });
+
+    const data = result.json();
+    return data.items;
+  };
+
+  //Get track info
+  const getTrack = async (token, trackEndPoint) => {
+    const result = await fetch(`${trackEndPoint}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer" + token },
+    });
+
+    const data = await result.json();
+    return data;
+  };
+
+  return {
+    getToken() {
+      return getToken();
+    },
+    getGenres(token) {
+      return getGenres(token);
+    },
+    getPlaylistsForGenre(token, genreId) {
+      return getPlaylistsForGenre(token, genreId);
+    },
+    getTracksForPlaylist(token, tracksEndPoint) {
+      return getTracksForPlaylist(token, tracksEndPoint);
+    },
+    getTrack(token, trackEndPoint) {
+      return getTrack(token, trackEndPoint);
+    },
+  };
 })();
